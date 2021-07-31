6061f5884616546edff3dc82c298e03a5f0440d9
mayaepps <mepps@lion.lmu.edu>
2021-07-30 19:16:02 -0700

    Implemented geocoding to extrapolate the coordinates from the user's address.

--- Neighborcycle/Neighborcycle/View Controllers/SignUpViewController.m
+++ Neighborcycle/Neighborcycle/View Controllers/SignUpViewController.m
@@ -9,6 +9,7 @@
 #import <Parse/Parse.h>
 #import "SceneDelegate.h"
 #import "AppDelegate.h"
+#import <CoreLocation/CLGeocoder.h>
 
 @interface SignUpViewController ()
 
@@ -33,12 +34,36 @@
     // Do any additional setup after loading the view.
 }
 
-
 - (IBAction)didTapSignUp:(id)sender {
-    [self parseSignUp];
+    NSString *fullAddress = [NSString stringWithFormat: @"%@, %@, %@ %@", self.streetAddressField.text, self.cityField.text, self.stateField.text, self.zipCodeField.text];
+    [self getGeoPointFromAddress:fullAddress];
 }
 
-- (void) parseSignUp {
+
+// Gets the geopoint information from the address, before trying to register the user in Parse.
+- (void) getGeoPointFromAddress: (NSString*) fullAddress {
+    
+    // Get the coordinates that correspond to the address.
+    [[CLGeocoder new] geocodeAddressString:fullAddress completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        
+        if (placemarks != NULL) {
+            PFGeoPoint *geopoint = [PFGeoPoint new];
+            CLLocationCoordinate2D coordinate = placemarks[0].location.coordinate;
+            
+            geopoint.latitude = coordinate.latitude;
+            geopoint.longitude = coordinate.longitude;
+            [self parseSignUpWithGeoPoint: geopoint];
+            
+        } else {
+            //TODO: make an error message to display to the user
+            NSLog(@"Error: Unknown address");
+        }
+    }];
+}
+
+// Signs a new user up in Parse given a PFGeoPoint using the user's responses.
+- (void) parseSignUpWithGeoPoint: (PFGeoPoint*) geoPoint {
+    
     // Initialize a user object
     PFUser *newUser = [PFUser user];
         
@@ -46,7 +71,7 @@
     newUser.username = self.usernameField.text;
     newUser.email = self.emailField.text;
     newUser.password = self.passwordField.text;
-    newUser[@"location"] = [PFGeoPoint new];
+    newUser[@"location"] = geoPoint;
     newUser[@"phone_number"] = self.phoneNumberField.text;
     
     // Sign up asynchronously
