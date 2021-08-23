a90fb9cadd807f5b362c87a959e5941b7a2467fb
mayaepps <mepps@lion.lmu.edu>
2021-08-23 11:13:18 -0700

    Edit profile now changes the user's geopoint if they change their address

--- Neighborcycle/Neighborcycle/View Controllers/EditProfileViewController.m
+++ Neighborcycle/Neighborcycle/View Controllers/EditProfileViewController.m
@@ -37,12 +37,16 @@
 }
 
 - (IBAction)didTapSaveChanges:(id)sender {
-    PFUser *user = PFUser.currentUser;
+    [self getGeoPointFromAddress: self.addressTextField.text];
+}
+
+- (void) saveUserChanges: (PFUser*) user withGeoPoint: (PFGeoPoint*) geoPoint {
     
     user.username = self.usernameTextField.text;
     user.email = self.emailTextField.text;
     user[@"address"] = self.addressTextField.text;
     user[@"phone_number"] = self.phoneNumberTextField.text;
+    user[@"location"] = geoPoint;
     
     [user saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
         if (succeeded) {
@@ -52,6 +56,28 @@
         }
         if (error) {
             NSLog(@"Error saving the user's profile changes: %@", error.localizedDescription);
+        }
+    }];
+    
+}
+
+// Gets the geopoint information from the address, before trying to register the user in Parse.
+- (void) getGeoPointFromAddress: (NSString*) fullAddress {
+    
+    // Get the coordinates that correspond to the address.
+    [[CLGeocoder new] geocodeAddressString:fullAddress completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        
+        if (placemarks != NULL) {
+            PFGeoPoint *geopoint = [PFGeoPoint new];
+            CLLocationCoordinate2D coordinate = placemarks[0].location.coordinate;
+            
+            geopoint.latitude = coordinate.latitude;
+            geopoint.longitude = coordinate.longitude;
+            [self saveUserChanges: PFUser.currentUser withGeoPoint: geopoint];
+            
+        } else {
+            //TODO: make an error message to display to the user
+            NSLog(@"Error: Unknown address");
         }
     }];
 }
